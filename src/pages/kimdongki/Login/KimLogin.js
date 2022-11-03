7707b22f4a3b107a0649b6c038a0e28004e1e4f2
김동기 <voicek@gimdong-giui-MacBookAir.local>
2022-11-03 16:04:25 +0900

    ADD:login 진행중

--- src/pages/kimdongki/Login/KimLogin.js
+++ src/pages/kimdongki/Login/KimLogin.js
@@ -1,7 +1,31 @@
 import React from 'react';
-
+import './KimLogin.scss';
 function KimLogin() {
-  return <div>김동기 로그인페이지</div>;
+  return (
+    <main>
+      <div className="login_wrapper">
+        <div className="login_logo">
+          <h1>Westagram</h1>
+        </div>
+        <div className="login_container">
+          <input
+            className="login_email"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          ></input>
+          <input
+            type="password"
+            class="login_pw"
+            placeholder="비밀번호"
+          ></input>
+
+          <button className="login_btn">로그인</button>
+        </div>
+        <span className="passwordfind_link">
+          <a href="">비밀번호를 잊으셨나요?</a>
+        </span>
+      </div>
+    </main>
+  );
 }
 
 export default KimLogin;
