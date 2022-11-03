0465993c689b5e9368563daa3b0b61ae62edf379
jjongbum92 <jjongbum92@gmail.com>
2022-11-03 10:07:53 +0900

    ADD: westagram 폴더 이동

--- src/pages/kwakjongbum/Login/KwakLogin.js
+++ src/pages/kwakjongbum/Login/KwakLogin.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './KwakLogin.scss';
 
 function KwakLogin() {
-  return <div>곽종범 로그인페이지</div>;
+  return (
+    <div className="login-wrap">
+      <h1>westagram</h1>
+      <div className="form-wrap">
+        <div className="form-input">
+          <input
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            className="input-id"
+          />
+        </div>
+        <div className="form-input">
+          <input type="password" placeholder="비밀번호" className="input-pw" />
+        </div>
+        <Link to="/main">
+          <button type="button" className="btn-login" disabled="disabled">
+            <span>로그인</span>
+          </button>
+        </Link>
+      </div>
+      <div className="info">
+        <a target="_blank">비밀번호를 잊으셨나요?</a>
+      </div>
+    </div>
+  );
 }
 
 export default KwakLogin;
--- src/pages/kwakjongbum/Main/KwakMain.js
+++ src/pages/kwakjongbum/Main/KwakMain.js
@@ -1,7 +1,257 @@
 import React from 'react';
+import './KwakMain.scss';
 
 function KwakMain() {
-  return <div>곽종범 메인페이지</div>;
+  return (
+    <div className="wrap">
+      <div className="header">
+        <div className="inner">
+          <h1 className="logo">
+            <a>
+              <img src="/images/kwakjongbum/icon-insta.png" alt="인스타" />
+              westagram
+            </a>
+          </h1>
+          <div className="search-input">
+            <input type="text" placeholder="검색" />
+          </div>
+          <div className="link-area">
+            <button type="button" className="link-btn explore">
+              탐색
+            </button>
+            <button type="button" className="link-btn heart new">
+              좋아요
+            </button>
+            <button type="button" className="link-btn profile">
+              프로필
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="l-content">
+          <div className="feed-box">
+            <div className="feed-top">
+              <img src="/images/kwakjongbum/img1.jpg" alt="샘플" />
+              <span className="user-id">canon_mj</span>
+              <div className="btn-box">
+                <button type="button" className="btn-more">
+                  더보기
+                </button>
+              </div>
+            </div>
+            <div className="feed-img">
+              <img src="/images/kwakjongbum/img3.jpg" alt="샘플" />
+            </div>
+            <div className="feed-info">
+              <div className="btn-area">
+                <div className="l-btn">
+                  <span className="btn-like">
+                    <input type="checkbox" id="btnLike" />
+                    <label for="btnLike">좋아요</label>
+                  </span>
+                  <button type="button" className="btn-comment">
+                    댓글
+                  </button>
+                  <button type="button" className="btn-share">
+                    공유
+                  </button>
+                </div>
+                <span className="btn-bookmark">
+                  <input type="checkbox" id="btnBookmark" />
+                  <label for="btnBookmark">북마크</label>
+                </span>
+              </div>
+              <div className="state-area">
+                <span className="icon">
+                  <img src="/images/kwakjongbum/img2.jpg" alt="샘플" />
+                </span>
+                <p className="text">
+                  <span className="state-id">aineword</span>님 외{' '}
+                  <span className="num">10명</span>이 좋아합니다
+                </p>
+              </div>
+              <ul className="comment-area">
+                <li className="my">
+                  <span className="comment-id">canon_mj</span>
+                  <span className="comment-content">
+                    위워크에서 진행한 베이킹 클래스 너무 좋았습니다.
+                  </span>
+                  <button type="button" className="btn-txt">
+                    더 보기
+                  </button>
+                </li>
+                <li>
+                  <span className="comment-id">naceosecius</span>
+                  <span className="comment-content">거봐 좋았잖아~~~~</span>
+                  <div className="btn-wrap">
+                    <button type="button" className="btn-like on">
+                      좋아요
+                    </button>
+                    <button type="button" className="btn-delete">
+                      삭제
+                    </button>
+                  </div>
+                </li>
+                <li>
+                  <span className="comment-id">naceosecius</span>
+                  <span className="comment-content">testtesttesttesttest</span>
+                  <div className="btn-wrap">
+                    <button type="button" className="btn-like">
+                      좋아요
+                    </button>
+                    <button type="button" className="btn-delete">
+                      삭제
+                    </button>
+                  </div>
+                </li>
+              </ul>
+              <p className="time">
+                <span>42분</span>전
+              </p>
+            </div>
+          </div>
+          <div className="feed-input">
+            <input type="text" placeholder="댓글 달기..." />
+            <button type="button" className="btn-post" disabled>
+              게시
+            </button>
+          </div>
+        </div>
+        <div className="r-content">
+          <div className="profile-box">
+            <span className="img">
+              <img src="/images/kwakjongbum/wecode_logo.webp" />
+            </span>
+            <div className="box">
+              <p className="name">wecode_bootcamp</p>
+              <ul className="info">
+                <li>WeCode</li>
+                <li>위코드</li>
+              </ul>
+            </div>
+          </div>
+          <div className="line-box">
+            <div className="tit-area">
+              <span className="tit">스토리</span>
+              <button type="button" className="btn-all">
+                모두 보기
+              </button>
+            </div>
+            <ul className="user-list">
+              <li>
+                <span className="img">
+                  <img src="/images/kwakjongbum/img4.jpg" alt="샘플" />
+                </span>
+                <p className="id">_yum_S</p>
+                <p className="time">
+                  <span>16분</span>전
+                </p>
+              </li>
+              <li>
+                <span className="img">
+                  <img src="/images/kwakjongbum/img4.jpg" alt="샘플" />
+                </span>
+                <p className="id">drink_eat_drink</p>
+                <p className="time">
+                  <span>3시간</span>전
+                </p>
+              </li>
+              <li>
+                <span className="img">
+                  <img src="/images/kwakjongbum/img4.jpg" alt="샘플" />
+                </span>
+                <p className="id">hyukyc</p>
+                <p className="time">
+                  <span>20시간</span>전
+                </p>
+              </li>
+              <li>
+                <span className="img">
+                  <img src="/images/kwakjongbum/img4.jpg" alt="샘플" />
+                </span>
+                <p className="id">jminkeek</p>
+                <p className="time">
+                  <span>1일</span>전
+                </p>
+              </li>
+              <li>
+                <span className="img">
+                  <img src="/images/kwakjongbum/img4.jpg" alt="샘플" />
+                </span>
+                <p className="id">zzang</p>
+                <p className="time">
+                  <span>2일</span>전
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="line-box">
+            <div className="tit-area">
+              <span className="tit">회원님을 위한 추천</span>
+              <button type="button" className="btn-all">
+                모두 보기
+              </button>
+            </div>
+            <ul className="user-list">
+              <li>
+                <span className="img">
+                  <img src="/images/kwakjongbum/img5.jpg" alt="샘플" />
+                </span>
+                <p className="id">joaaaaahye</p>
+                <p className="txt">
+                  <span>_legend_a</span>님 외 <span>2</span>명이 팔로우 합니다.
+                </p>
+                <button type="button" className="btn-follow">
+                  팔로우
+                </button>
+              </li>
+              <li>
+                <span className="img">
+                  <img src="/images/kwakjongbum/img5.jpg" alt="샘플" />
+                </span>
+                <p className="id">rampart81</p>
+                <p className="txt">
+                  <span>ringo.in.seoul</span>님 외 <span>12</span>명이 팔로우
+                  합니다.
+                </p>
+                <button type="button" className="btn-follow">
+                  팔로우
+                </button>
+              </li>
+              <li>
+                <span className="img">
+                  <img src="/images/kwakjongbum/img5.jpg" alt="샘플" />
+                </span>
+                <p className="id">shawnjjoo</p>
+                <p className="txt">
+                  <span>jimmylee1220</span>님 외 <span>18</span>명이 팔로우
+                  합니다.
+                </p>
+                <button type="button" className="btn-follow">
+                  팔로우
+                </button>
+              </li>
+            </ul>
+          </div>
+          <ul className="info-txt">
+            <li>Instagram 정보</li>
+            <li>지원</li>
+            <li>홍보 센터</li>
+            <li>API</li>
+            <li>채용 정보</li>
+            <li>개인정보처리방침</li>
+            <li>약관</li>
+            <li>디렉터리</li>
+            <li>프로필</li>
+            <li>해시태그</li>
+            <li>언어</li>
+          </ul>
+          <p className="cop">ⓒ 2019 INSTAGRAM</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default KwakMain;
