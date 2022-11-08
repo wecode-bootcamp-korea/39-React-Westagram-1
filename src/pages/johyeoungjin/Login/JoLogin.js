05fe8c517557bca2d02b93bca6b5d46731e833b8
조형진 <98885221+gowell032@users.noreply.github.com>
2022-11-08 17:03:30 +0900

    asd

--- src/pages/johyeoungjin/Login/JoLogin.js
+++ src/pages/johyeoungjin/Login/JoLogin.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './JoLogin.scss';
 
 function JoLogin() {
-  return;
+  const [isIdValue, setIdValue] = useState();
+  const saveUserId = event => {
+    setIdValue(event.target.value);
+  };
+  return (
+    <div className="login-container">
+      <div className="container">
+        <div className="loginBox">
+          <h1 className="title">westagram</h1>
+
+          <input
+            className="idForm"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={saveUserId}
+          />
+          <input
+            className="pwForm"
+            type="password"
+            placeholder="비밀번호"
+            onChange={saveUserId}
+          />
+
+          <button className="loginButton">로그인</button>
+          <a href="#">
+            <p>비밀번호를 잊으셨나요?</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default JoLogin;
--- src/pages/johyeoungjin/Main/JoMain.js
+++ src/pages/johyeoungjin/Main/JoMain.js
@@ -1,7 +1,82 @@
 import React from 'react';
+import './JoMain.scss';
 
 function JoMain() {
-  return <div>조형진 메인페이지</div>;
+  return (
+    <>
+      <div className="main-container">
+        <div className="container">
+          <div className="nav">
+            <div className="barLeft">
+              <i className="fa-brands fa-instagram"></i>
+              <span className="title">westagram</span>
+            </div>
+
+            <div className="navSearchFrame">
+              <i class="fa-solid fa-magnifying-glass"></i>
+              <input className="navSearch" type="text" placeholder="검색" />
+            </div>
+
+            <div className="barRight">
+              <i className="fa-regular fa-compass navCompass"></i>
+              <i className="fa-regular fa-heart navHeart"></i>
+              <i className="fa-regular fa-user navUser"></i>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="feeds">
+            <div className="article">
+              <div className="feedsBar">
+                <img
+                  className="proImg"
+                  src="/images/johyeoungjin/charlesdeluvio-Mv9hjnEUHR4-unsplash.png"
+                  art="dog"
+                />
+                <span className="nickName">underdog</span>
+                <i className="fa-solid fa-ellipsis"></i>
+              </div>
+              <img
+                className="mainImg"
+                src="/images/johyeoungjin/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg"
+                alt="dog"
+              />
+
+              <div className="likeBar">
+                <i className="fa-solid fa-heart likeBarHeart"></i>
+                <i className="fa-regular fa-message"></i>
+                <i className="fa-solid fa-arrow-up-from-bracket bracket"></i>
+                <i className="fa-regular fa-bookmark bookmark"></i>
+              </div>
+
+              <div className="likeBar2">
+                <img
+                  className="likeImg"
+                  src="/images/johyeoungjin/charlesdeluvio-Mv9hjnEUHR4-unsplash.png"
+                  alt="dog"
+                />
+                <span className="likeComent">
+                  <span className="nickNameBold">underdog</span>님
+                  <span className="nickNameBold">외 962명</span>이 좋아합니다
+                </span>
+              </div>
+              <div className="articleComentFrame">
+                <input
+                  className="commentInput"
+                  type={'text'}
+                  placeholder="댓글달기..."
+                />
+              </div>
+            </div>
+          </div>
+          <div className="main-right">
+            <div className="main-right-top"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default JoMain;
