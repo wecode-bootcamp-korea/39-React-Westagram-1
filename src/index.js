4a0c6f724f46f3167de51ae6756d385885028c09
김동기 <voicek@gimdong-giui-MacBookAir.local>
2022-11-09 14:17:57 +0900

    ADD:로그인 활성화,메인페이지 수정중

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles/common.scss';
 import './pages/kimdongki/Login/KimLogin.scss';
 import './pages/kimdongki/Main/KimMain.scss';
 import './components/Nav/Nav.scss';
+import 'https://kit.fontawesome.com/51d8702035.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
--- src/pages/kimdongki/Login/KimLogin.js
+++ src/pages/kimdongki/Login/KimLogin.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './KimLogin.scss';
 
 function KimLogin() {
-  const navi = useNavigate();
-  const moveMain = () => {
-    navi('./KimMain');
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/KimMain');
   };
+
+  const [id, idValue] = useState('');
+  const [pw, pwValue] = useState('');
+
+  const saveUserId = e => {
+    idValue(e.target.value);
+  };
+
+  const saveUserPw = e => {
+    pwValue(e.target.value);
+  };
+
+  const loginValid = id && id.includes('@') && pw && pw.length >= 5;
+
+  const pressEnter = e => {
+    if (e.code === 'Enter') {
+      goToMain();
+    }
+  };
+
   return (
     <main>
       <div className="login_wrapper">
@@ -15,17 +35,25 @@ function KimLogin() {
         </div>
         <div className="login_container">
           <input
+            onChange={saveUserId}
             type="text"
             className="login_email"
             placeholder="전화번호, 사용자 이름 또는 이메일"
-          ></input>
+          />
           <input
+            onChange={saveUserPw}
+            onKeyDown={pressEnter}
             type="password"
-            class="login_pw"
+            className="login_pw"
             placeholder="비밀번호"
-          ></input>
+          />
 
-          <button onClick={moveMain} className="login_btn">
+          <button
+            className="login_btn"
+            // type="button"
+            onClick={goToMain}
+            disabled={!loginValid ? true : false}
+          >
             로그인
           </button>
         </div>
