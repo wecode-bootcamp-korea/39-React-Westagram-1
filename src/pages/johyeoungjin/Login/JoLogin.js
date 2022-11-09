ae7f255e654b5006e866b9bc755fd7a18ff2ca47
조형진 <98885221+gowell032@users.noreply.github.com>
2022-11-09 17:14:49 +0900

    Add Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/johyeoungjin/Login/JoLogin.js
+++ src/pages/johyeoungjin/Login/JoLogin.js
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import './JoLogin.scss';
 
 function JoLogin() {
-  const [isIdValue, setIdValue] = useState();
+  const [idValue, setIdValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
+
   const saveUserId = event => {
+    console.log(idValue);
     setIdValue(event.target.value);
   };
+  const saveUserPw = event => {
+    console.log(pwValue);
+    setPwValue(event.target.value);
+  };
+
+  const isValid = pwValue.length > 5 && idValue.includes('@');
+
   return (
     <div className="login-container">
       <div className="container">
@@ -15,17 +25,23 @@ function JoLogin() {
           <input
             className="idForm"
             type="text"
+            name="id"
             placeholder="전화번호, 사용자 이름 또는 이메일"
             onChange={saveUserId}
+            value={idValue}
           />
           <input
             className="pwForm"
+            name="pw"
             type="password"
             placeholder="비밀번호"
-            onChange={saveUserId}
+            onChange={saveUserPw}
+            value={pwValue}
           />
 
-          <button className="loginButton">로그인</button>
+          <button className="loginButton" disabled={!isValid}>
+            로그인
+          </button>
           <a href="#">
             <p>비밀번호를 잊으셨나요?</p>
           </a>
